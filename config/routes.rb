13c0763b89d7989b35e55661e8bf48975d72586e
yui-0326 <maeda.yuiha.infratop@gmail.com>
2021-05-29 08:33:44 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  get 'top' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
