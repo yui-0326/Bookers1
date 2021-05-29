5db95b1bf5a397a1564b95c1a76a12e6ffd28193
yui-0326 <maeda.yuiha.infratop@gmail.com>
2021-05-29 11:54:36 +0000

    create create function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
   def index
-    @book = Book
+    @book = Book.new
   end
 
   def show
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
