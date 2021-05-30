ab86fd9dbae8df934846ccb73a2080f51db3ac55
yui-0326 <maeda.yuiha.infratop@gmail.com>
2021-05-30 04:16:38 +0000

    create destroy function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,12 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   private
   def book_params
