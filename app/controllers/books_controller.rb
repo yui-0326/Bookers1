0779ebb28ed01f2efb205724a309b148b6ab2a6b
yui-0326 <maeda.yuiha.infratop@gmail.com>
2021-05-30 05:56:06 +0000

    create flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,10 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    end
   end
 
   def edit
@@ -20,14 +22,18 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = 'Book was successfully destroyed.'
+      redirect_to books_path
+    end
   end
 
   private
